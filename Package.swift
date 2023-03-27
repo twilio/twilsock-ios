812e11cdf007c8e06005259fda5c21992ea67cdc
Distiller <distiller@static.198.206.135.33.macminivault.com>
2023-03-27 09:42:51 +0000

    Update Package.swift to 1.0.0

--- Package.swift
+++ Package.swift
@@ -24,17 +24,17 @@ let package = Package(
         .binaryTarget(
             name: "TwilioTwilsockLib",
             url: "https://sdk.twilio.com/ios/twilsock/releases/1.0.0/twilio-twilsock-ios-1.0.0.zip",
-            checksum: "181b4e8960ade2bc7b335068a82c82dd9ce3506f28f9ed27b182d593085cb3f5"
+            checksum: "f783cedb93fa7268dde1a22266431ebd0b275d36d0245dd6a5389582bae18402"
         ),
         .binaryTarget(
             name: "TwilioCommonLib",
             url: "https://sdk.twilio.com/ios/twilsock/releases/1.0.0/twilio-commonLib-ios-1.0.0.zip",
-            checksum: "8ab90dc83f38bcad4a30c42b6b64c7a4007aa0a82ccfdfcda47c7ecdeb41db51"
+            checksum: "2cd6b96c36a680a1511ca48da076e43a294f8fd99ed51a73a61b07a9630e71af"
         ),
         .binaryTarget(
             name: "TwilioStateMachine",
             url: "https://sdk.twilio.com/ios/twilsock/releases/1.0.0/twilio-stateMachine-ios-1.0.0.zip",
-            checksum: "381ed0b8dba859aed13b0e0a72820a34cbe6e82c5fb516ad2d87691640e942a1"
+            checksum: "75f5307e48b0d9bf9436d1d73b66eb7f63c81ccaf0364d103db2d011c8430920"
         )
     ]
 )
