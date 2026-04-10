838b58fb9e52c7720d29b27bbf1c97b820ead29e
Cong Shi <cshi@squareup.com>
2026-04-10 00:06:09 -0700

    Fix a case sensitivity issue for cross-platform compatibility
    Package.swift references path Dummy but the actual directory is dummy. It causes build error on case sensitve platform.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
               .target(name: "TwilioCommonLib"),
               .target(name: "TwilioStateMachine") 
           ],
-          path: "Dummy"
+          path: "dummy"
         ),
         .binaryTarget(
             name: "TwilioTwilsockLib",
